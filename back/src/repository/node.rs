8a1fbaf299f923d1ac2397237580f2a01634fa7c
n.yokoyama <naoya.yokoyama.se@gmail.com>
2026-09-17 05:48:22 +0900

    WIP: add node favorite to Home

--- back/src/repository/node.rs
+++ back/src/repository/node.rs
@@ -1,4 +1,4 @@
-use crate::model::note::Note;
+use crate::model::node::Node;
 use rusqlite::{Connection, OptionalExtension, Result, params, params_from_iter};
 use std::collections::HashSet;
 
@@ -11,9 +11,7 @@ pub fn find_all_paths(conn: &Connection) -> Result<HashSet<String>> {
     ";
 
     let mut stmt = conn.prepare(sql)?;
-
     let paths = stmt.query_map([], |row| row.get::<_, String>(0))?;
-
     paths.collect()
 }
 
@@ -28,6 +26,45 @@ pub fn find_node_id_by_node_path(conn: &Connection, path: &str) -> Result<Option
     ";
 
     conn.query_row(sql, [path], |row| row.get(0)).optional()
+}
+
+pub fn find_by_node_ids(conn: &Connection, node_ids: &[String]) -> Result<Vec<Node>> {
+    if node_ids.is_empty() {
+        return Ok(Vec::new());
+    }
+
+    let placeholders = vec!["?"; node_ids.len()].join(",");
+
+    let sql = format!(
+        "
+        SELECT
+          node_id,
+          path,
+          updated_at
+        FROM
+          node
+        WHERE
+          node_id IN ({})
+        ",
+        placeholders
+    );
+
+    let mut stmt = conn.prepare(&sql)?;
+
+    let nodes = stmt
+        .query_map(params_from_iter(node_ids), |row| {
+            Ok(Node {
+                node_id: row.get(0)?,
+                path: row.get(1)?,
+                description: String::new(),
+                status: String::new(),
+                updated_by: String::new(),
+                updated_at: row.get(2)?,
+            })
+        })?
+        .collect::<Result<Vec<_>>>()?;
+
+    Ok(nodes)
 }
 
 pub fn create_node(conn: &Connection, node_id: &str, path: &str) -> Result<i64> {
--- back/src/service/home.rs
+++ back/src/service/home.rs
@@ -1,14 +1,15 @@
 use crate::{
     api::home::response::{FavoriteItem, FavoriteListResponse, SearchItem, SearchResponse},
-    repository::favorite as favorite_repository,
-    repository::note as note_repository,
-    repository::wiki as wiki_repository,
+    repository::favorite as favorite_repo,
+    repository::node as node_repo,
+    repository::note as note_repo,
+    repository::wiki as wiki_repo,
 };
 use rusqlite::Connection;
 
 pub fn search(conn: &Connection, keyword: &str) -> rusqlite::Result<SearchResponse> {
-    let notes = note_repository::find_by_search(conn, keyword)?;
-    let wikis = wiki_repository::find_by_search(conn, keyword)?;
+    let notes = note_repo::find_by_search(conn, keyword)?;
+    let wikis = wiki_repo::find_by_search(conn, keyword)?;
     let mut search_items: Vec<SearchItem> = Vec::new();
 
     search_items.extend(notes.into_iter().map(|note| SearchItem {
@@ -34,24 +35,35 @@ pub fn get_favorite_list(
     conn: &Connection,
     user_id: &str,
 ) -> rusqlite::Result<FavoriteListResponse> {
-    let favorites = favorite_repository::find_by_user_id(conn, user_id)?;
-    let mut note_ids = Vec::new();
+    let favorites = favorite_repo::find_by_user_id(conn, user_id)?;
     let mut node_ids = Vec::new();
+    let mut note_ids = Vec::new();
     let mut wiki_ids = Vec::new();
     for favorite in favorites {
-        if !favorite.note_id.is_empty() {
-            note_ids.push(favorite.note_id);
-        }
         if !favorite.node_id.is_empty() {
             node_ids.push(favorite.node_id);
+        }
+        if !favorite.note_id.is_empty() {
+            note_ids.push(favorite.note_id);
         }
         if !favorite.wiki_id.is_empty() {
             wiki_ids.push(favorite.wiki_id);
         }
     }
-    let notes = note_repository::find_by_note_ids(conn, &user_id, &note_ids)?;
-    let wikis = wiki_repository::find_by_wiki_ids(conn, &user_id, &wiki_ids)?;
+
+    let nodes = node_repo::find_by_node_ids(conn, &node_ids)?;
+    let notes = note_repo::find_by_note_ids(conn, &user_id, &note_ids)?;
+    let wikis = wiki_repo::find_by_wiki_ids(conn, &user_id, &wiki_ids)?;
     let mut favorite_list: Vec<FavoriteItem> = Vec::new();
+
+    favorite_list.extend(nodes.into_iter().map(|node| FavoriteItem {
+        favorite_id: "".to_string(),
+        name: node.path,
+        node_id: node.node_id,
+        note_id: "".to_string(),
+        wiki_id: "".to_string(),
+    }));
+
     favorite_list.extend(notes.into_iter().map(|note| FavoriteItem {
         favorite_id: "".to_string(),
         name: note.title,
